a4b3ca935687843280d3f8133d7525830bd1d920
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2022-03-18 23:02:37 -0300

    cosas

--- proyecto-practica-marketplace/src/App.tsx
+++ proyecto-practica-marketplace/src/App.tsx
@@ -14,8 +14,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <Component2 />
         <Component1 />
+        <Component2 />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
